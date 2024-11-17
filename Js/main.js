654f98c762f7ab611f416991115b0d2f0ee475f2
ms.moradi <ms.moradi47@gmail.com>
2024-11-17 12:32:13 +0330

    Getting and saving elements with the IDs code, key, location, which

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,10 @@
 let containerText = document.querySelector('.container-text');
 let containerDetails = document.querySelector('.container-details');
 
+let key = document.querySelector('#key');
+let location = document.querySelector('#location');
+let which = document.querySelector('#which');
+let code = document.querySelector('#code');
 
 document.body.addEventListener('keydown', function(event){
 
